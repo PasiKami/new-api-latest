d80a1079df74709faa740ba29d3e84e61c02e38f
KamiPasi <1428727144@qq.com>
2025-02-07 11:53:53 +0800

    fix: 更新模型比例，调整 o1-mini 和 o3-mini 相关值，并添加 deepseek-r1 比例

--- common/model-ratio.go
+++ common/model-ratio.go
@@ -50,8 +50,16 @@ var defaultModelRatio = map[string]float64{
 	"o1-2024-12-17":                   7.5,
 	"o1-preview":                      7.5,
 	"o1-preview-2024-09-12":           7.5,
-	"o1-mini":                         1.5,
-	"o1-mini-2024-09-12":              1.5,
+	"o1-mini":                         0.55,
+	"o1-mini-2024-09-12":              0.55,
+	"o3-mini":                         0.55,
+	"o3-mini-2025-01-31":              0.55,
+	"o3-mini-high":                    0.55,
+	"o3-mini-2025-01-31-high":         0.55,
+	"o3-mini-low":                     0.55,
+	"o3-mini-2025-01-31-low":          0.55,
+	"o3-mini-medium":                  0.55,
+	"o3-mini-2025-01-31-medium":       0.55,
 	"gpt-4o-mini":                     0.075,
 	"gpt-4o-mini-2024-07-18":          0.075,
 	"gpt-4-turbo":                     5,    // $0.01 / 1K tokens
@@ -181,6 +189,7 @@ var defaultModelRatio = map[string]float64{
 	"command-r-plus-08-2024": 1.25,
 	"deepseek-chat":          0.07,
 	"deepseek-coder":         0.07,
+	"deepseek-r1":            0.275,
 	// Perplexity online 模型对搜索额外收费，有需要应自行调整，此处不计入搜索费用
 	"llama-3-sonar-small-32k-chat":   0.2 / 1000 * USD,
 	"llama-3-sonar-small-32k-online": 0.2 / 1000 * USD,
@@ -339,6 +348,10 @@ func UpdateCompletionRatioByJSONString(jsonStr string) error {
 }
 
 func GetCompletionRatio(name string) float64 {
+	if ratio, ok := CompletionRatio[name]; ok {
+		return ratio
+	}
+	lowercaseName := strings.ToLower(name)
 	if strings.HasPrefix(name, "gpt-4-gizmo") {
 		name = "gpt-4-gizmo-*"
 	}
@@ -401,7 +414,10 @@ func GetCompletionRatio(name string) float64 {
 			return 2
 		}
 	}
-	if strings.HasPrefix(name, "deepseek") {
+	if strings.HasPrefix(lowercaseName, "deepseek") {
+		if strings.Contains(lowercaseName, "r") {
+			return 4
+		}
 		return 2
 	}
 	if strings.HasPrefix(name, "ERNIE-Speed-") {
@@ -420,9 +436,6 @@ func GetCompletionRatio(name string) float64 {
 		return 2
 	case "llama3-70b-8192":
 		return 0.79 / 0.59
-	}
-	if ratio, ok := CompletionRatio[name]; ok {
-		return ratio
 	}
 	return 1
 }

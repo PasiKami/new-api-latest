a2316a6569d88280141d760ad96012c6e5a9d110
KamiPasi <1428727144@qq.com>
2025-02-12 16:51:48 +0800

    fix: 在 ConvertImageUrlsToBase64 函数中添加条件判断，避免处理字符串内容

--- service/image.go
+++ service/image.go
@@ -183,6 +183,9 @@ func getImageConfig(reader io.Reader) (image.Config, string, error) {
 }
 
 func ConvertImageUrlsToBase64(m *dto.Message, userId int) {
+	if m.IsStringContent() {
+		return
+	}
 	contentList := m.ParseContent()
 	for i, cItem := range contentList {
 		if cItem.Type == dto.ContentTypeImageURL {
